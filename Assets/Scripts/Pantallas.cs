adb44d045ffc227e47896a6708dbfe1ec9b2df47
LostError01 <ishi@live.com.mx>
2025-10-25 01:45:43 -0600

    Minijuego03

--- Assets/Scripts/Pantallas.cs
+++ Assets/Scripts/Pantallas.cs
@@ -4,15 +4,33 @@ public class Pantallas : MonoBehaviour
 {
     [Header("Animators")]
     [SerializeField] private Animator animatorPantalla1;
+
+    private int pantallaActual = 0;
+
+    private void Update()
+    {
+        pantallaActual = animatorPantalla1.GetInteger("Pantalla");
+    }
     public void PantallaSig()
     {
-        if(animatorPantalla1.GetInteger("Pantalla") == 0)
+        if(pantallaActual == 0)
         animatorPantalla1.SetInteger("Pantalla", 1);
+
+        if(pantallaActual == -1)
+            animatorPantalla1.SetInteger("Pantalla", 0);
     }
 
     public void PantallaAnt()
     {
-        if(animatorPantalla1.GetInteger("Pantalla") == 1)
-        animatorPantalla1.SetInteger("Pantalla", 0);
+        if (pantallaActual == 1)
+        {
+            animatorPantalla1.SetInteger("Pantalla", 0);
+        }
+        
+        if (pantallaActual == 0)
+        {
+            animatorPantalla1.SetInteger("Pantalla", -1);
+        }
+
     }
 }
